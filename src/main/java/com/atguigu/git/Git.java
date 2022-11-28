96cc63a7fd1fb310d836b03cee809c2e656aeee5
wuwenlei <2932771620@qq.com>
2022-11-28 10:12:52 +0800

    hot-fix提交

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -5,5 +5,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git");
+        System.out.println("hell0,git1");
+        System.out.println("hello,git4");
     }
 }
