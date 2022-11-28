e40d468c2c7b520ba6dc6c01a030006e87b51860
默默加油的小孩子 <98151667+2932771620@users.noreply.github.com>
2022-11-28 18:11:46 +0800

    pull commit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -8,6 +8,7 @@ public class Git {
         System.out.println("hell0,git1");
         System.out.println("hello,git4");
         System.out.println("master test");
+        System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("hot-fix test");
 
